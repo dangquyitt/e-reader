ac46841d2efbfa82341a68f59b539d5d5b6000e4
Nguyen Dang Quy <dangquyitt@gmail.com>
2024-10-02 21:56:14 +0700

    initial project

---
+++ src/main/java/utc2/itk62/e_reader/EReaderApplication.java
@@ -0,0 +1,13 @@
+package utc2.itk62.e_reader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EReaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EReaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/utc2/itk62/e_reader/EReaderApplicationTests.java
@@ -0,0 +1,13 @@
+package utc2.itk62.e_reader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EReaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
