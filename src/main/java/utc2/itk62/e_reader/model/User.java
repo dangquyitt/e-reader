4eafc13dcbd28fd07ceea5ba53a8524e78c8c249
Nguyen Dang Quy <dangquyitt@gmail.com>
2024-10-02 23:05:37 +0700

    Added docker compose file to start postgres service

---
+++ src/main/java/utc2/itk62/e_reader/model/User.java
@@ -0,0 +1,22 @@
+package utc2.itk62.e_reader.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String email;
+}
