8bb805795e3b7d2b282a314417c684be29fbba37
ducphong <doducphong21062003@gmail.com>
2024-10-21 23:19:09 +0700

    Add feature to save access token in cookie

--- src/main/java/utc2/itk62/e_reader/service/BookService.java
+++ src/main/java/utc2/itk62/e_reader/service/BookService.java
@@ -1,10 +1,8 @@
 package utc2.itk62.e_reader.service;
 
-import org.springframework.web.multipart.MultipartFile;
 import utc2.itk62.e_reader.domain.entity.Book;
 import utc2.itk62.e_reader.domain.model.CreateBookParam;
 import utc2.itk62.e_reader.domain.model.UpdateBookParam;
-import utc2.itk62.e_reader.dto.CreateBookRequest;
 
 import java.util.List;
 
@@ -13,7 +11,7 @@ public interface BookService {
     Book createBook(CreateBookParam createBookParam);
     boolean deleteBook(Long id);
     Book updateBook(UpdateBookParam updateBookParam);
-
     Book getBook(Long id);
+
     List<Book> getAllBook();
 }
