196860d4640474da26ea9d8a04f30d70f07342e8
Nguyen Dang Quy <dangquyitt@gmail.com>
2025-01-05 16:58:25 +0700

    filter and sort price

--- src/main/java/utc2/itk62/e_reader/domain/model/PriceFilter.java
+++ src/main/java/utc2/itk62/e_reader/domain/model/PriceFilter.java
@@ -2,8 +2,10 @@ package utc2.itk62.e_reader.domain.model;
 
 import lombok.Data;
 
+import java.util.List;
+
 
 @Data
 public class PriceFilter {
-    Long planId;
+    List<Long> planIds;
 }
--- src/main/java/utc2/itk62/e_reader/service/PriceService.java
+++ src/main/java/utc2/itk62/e_reader/service/PriceService.java
@@ -2,6 +2,7 @@ package utc2.itk62.e_reader.service;
 
 import utc2.itk62.e_reader.core.pagination.Pagination;
 import utc2.itk62.e_reader.domain.entity.Price;
+import utc2.itk62.e_reader.domain.model.OrderBy;
 import utc2.itk62.e_reader.domain.model.PriceFilter;
 import utc2.itk62.e_reader.dto.price.CreatePriceRequest;
 
@@ -10,5 +11,5 @@ import java.util.List;
 public interface PriceService {
     Long createPrice(CreatePriceRequest createPriceRequest);
 
-    List<Price> getListPrice(PriceFilter filter, Pagination pagination);
+    List<Price> getListPrice(PriceFilter filter, OrderBy orderBy, Pagination pagination);
 }
